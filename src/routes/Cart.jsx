347efbfd1d8ee67116aee108cfc165346a72c5a2
joonaro <cupetus@gmail.com>
2024-01-26 18:48:33 +0200

    added cartProducts to cart

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+import ProductSingle from "./ProductSingle";
 const Cart = () => {
+    const cartProducts = useSelector((state) => state.cart.cart);
+    console.log(cartProducts);
     return (
         <div>
             <h1>Cart</h1>
+            {cartProducts.map((product) => (
+                <ProductSingle key={product.id} {...product} />
+            ))}
         </div>
     );
 }
