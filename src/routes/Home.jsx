596aff7eadc857f22e015b03301cf9334dff4c04
joonaro <cupetus@gmail.com>
2024-01-26 12:19:08 +0200

    halfway

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,5 @@
 import { Typography } from "@mui/material";
+import { Button } from "react-bootstrap";
 
 const Home = () => {
   return (
@@ -6,6 +7,7 @@ const Home = () => {
       <Typography variant="h6" component="h2">
         Home
       </Typography>
+      <Button variant="primary">Example Button</Button>
     </div>
   );
 };
